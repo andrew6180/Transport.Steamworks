442d44c8da4c1386deada3a0c3f6d9e8c56f7ceb
aseward <andrewseward93@gmail.com>
2019-02-13 11:44:43 -0700

    added default constructors for exceptions

--- Exceptions.cs
+++ Exceptions.cs
@@ -5,21 +5,29 @@ namespace Transport.Steamworks
     public class ConnectionTimeoutException : Exception
     {
         public ConnectionTimeoutException(string message) : base(message) { }
+
+        public ConnectionTimeoutException() : base("The connection to the remote user timed out.") { }
     }
 
     public class NoSteamConnectionException : Exception
     {
         public NoSteamConnectionException(string message) : base(message) { }
+
+        public NoSteamConnectionException() : base("Could not connect to steam.") { }
     }
 
     public class SteamConnectionException : Exception
     {
         public SteamConnectionException(string message) : base(message) { }
+
+        public SteamConnectionException() : base("Unknown Steam Connection Error.") { }
     }
 
     public class BadMethodCallException : Exception
     {
         public BadMethodCallException(string message) : base(message) { }
+        
+        public BadMethodCallException() : base("Method called in an unintended way") { }
     }
 
     public class BadChannelException : Exception
